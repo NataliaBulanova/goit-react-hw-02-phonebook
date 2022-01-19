3dd3c0a70c305d151699cccdae9260936a4aacdb
NataliaBulanova <nbulanova91@gmail.com>
2022-01-19 19:59:23 +0200

    styles correction

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -3,10 +3,12 @@ import styled from "styled-components";
 export const List = styled.ul`
   list-style: none;
   padding: 0px;
-  min-height: 160px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+
+  & > li {
+    margin-bottom: 10px;
+  }
 `;
 
 export const ContactText = styled.span`
